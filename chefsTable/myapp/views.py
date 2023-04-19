81201d9413ec5554c13b069aa76b0bb9f0bf7f11
rjanant <53167490+rjanant@users.noreply.github.com>
2023-04-19 17:57:04 +0530

    chefsTable App
    chefsTable is a simple app designed for learning basic django functionalities.

---
+++ chefsTable/myapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from datetime import  datetime
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Welcome to Bollywood Restaurant!')
+
+def display_date(request):
+    date_joined = datetime.today().year
+    return HttpResponse(date_joined)
+
+def menu(request):
+    text = """<h1 style = "color: #F4CE14;"> This is Bollywood Restaurant</h1>"""
+    return HttpResponse(text)
+
+def drinks(request,drink_name):
+    drink = {
+        'mocha' : 'type of coffee',
+        'tea' : 'type of beverage',
+        'lemonade' : 'type of refreshment'
+    }
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f"<h2> {drink_name} <h2>" + choice_of_drink)
+
